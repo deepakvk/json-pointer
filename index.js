28081b48217756d606cbf2e2d1f5744f28b5b3a1
dvijayakumar <dvijayakumar@axway.com>
2021-11-23 10:15:33 +0000

    Fix for prototype pollution CVE-2021-23820

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 'use strict';
 
-var each = require('foreach');
+// var each = require('foreach');
 module.exports = api;
 
+
+var hasOwn = Object.prototype.hasOwnProperty;
+var toString = Object.prototype.toString;
+
+function each (obj, fn, ctx) {
+    if (toString.call(fn) !== '[object Function]') {
+        throw new TypeError('iterator must be a function');
+    }
+    var l = obj.length;
+    if (l === +l) {
+        for (var i = 0; i < l; i++) {
+            fn.call(ctx, obj[i], i, obj);
+        }
+    } else {
+        for (var k in obj) {
+            if (hasOwn.call(obj, k)) {
+                fn.call(ctx, obj[k], k, obj);
+            }
+        }
+    }
+};
 
 /**
  * Convenience wrapper around the api.
@@ -75,6 +96,9 @@ api.set = function set (obj, pointer, value) {
 
     for (var i = 0; i < refTokens.length - 1; ++i) {
         var tok = refTokens[i];
+        if (typeof tok !== 'string' && typeof tok !== 'number') {
+            tok = String(tok);
+          }
         if (tok === "__proto__" || tok === "constructor" || tok === "prototype") {
             continue
         }
